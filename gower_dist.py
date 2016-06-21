3ba29e6abdf3fc83fbf17f2006b4de35642821e2
Gregory Matchado <gregorymatchado@gmail.com>
2016-06-21 11:24:28 -0700

    Create gower_dist.py

---
+++ gower_dist.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import DistanceMetric
+
+
+def gower_distance(X):
+    """
+    This function expects a pandas dataframe as input
+    The data frame is to contain the meta-features of customers. Based on these meta-features a
+    distance matrix will be returned which will contain the pairwise gower distance between the customers
+    All variables of object type will be treated as nominal variables and the others will be treated as 
+    numeric variables.
+
+    Distance metrics used for:
+
+    Nominal variables: Dice distance (https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient)
+    Numeric variables: Manhattan distance normalized by the range of the variable (https://en.wikipedia.org/wiki/Taxicab_geometry)
+    """
+    individual_variable_distances = []
+
+    for i in range(X.shape[1]):
+        feature = X.iloc[:,[i]]
+        if feature.dtypes[0] == np.object:
+            feature_dist = DistanceMetric.get_metric('dice').pairwise(pd.get_dummies(feature))
+        else:
+            feature_dist = DistanceMetric.get_metric('manhattan').pairwise(feature) / np.ptp(feature.values)
+            
+        individual_variable_distances.append(feature_dist)
+
+    return np.array(individual_variable_distances).mean(0)
